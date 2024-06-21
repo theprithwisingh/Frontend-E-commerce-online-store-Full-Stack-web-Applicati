5451940eee7718fdf25f4cb823eea18770188380
theprithwisingh <prithwisingh77@gmail.com>
2024-06-21 23:45:26 +0530

    hero/Item/Popular/Pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 function Navbar() {
-  const [menu, setMenu]  =useState("shop")
+  const [menu, setMenu]  =useState("shop")// hr tag 
   return (
     <div className='navbar'>
        <div className='nav-logo'>
@@ -11,14 +12,14 @@ function Navbar() {
          <p>PRITHWI</p>
        </div>
        <ul className='nav-menu'>
-         <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-         <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
-         <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
-         <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+         <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{ textDecoration: 'none' }}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+         <li onClick={()=>{setMenu("mens")}}><Link to='/mens' style={{ textDecoration: 'none' }}>Mens</Link>{menu==="mens"?<hr/>:<></>}</li>
+         <li onClick={()=>{setMenu("womens")}}><Link to='/womens' style={{ textDecoration: 'none' }}>Womens</Link>{menu==="womens"?<hr/>:<></>}</li>
+         <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{ textDecoration: 'none' }}>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
        </ul>
        <div className='nav-login-cart'>
-        <button>Login</button>
-        <img src={cart_icon} alt="" />
+       <Link to='/login'><button>Login</button></Link>
+       <Link to='/cart'><img src={cart_icon} alt=""/></Link>
         <div className='nav-cart-count'>0</div>
        </div>
     </div>

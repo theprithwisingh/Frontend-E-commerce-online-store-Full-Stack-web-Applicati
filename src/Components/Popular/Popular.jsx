b03c8d2c3fd009eb302062ba0931774a1809cd18
theprithwisingh <prithwisingh77@gmail.com>
2024-06-22 21:13:07 +0530

    new collection component

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -8,8 +8,8 @@ const Popular = () => {
     <h1>Popular In Women</h1>
      <hr />
      <div className='popular_item'>
-      {data_product.map((item)=>{
-        return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+      {data_product.map((item,i)=>{
+        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
       })}
      </div>
    </div>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero';
 import Popular from '../Components/Popular/Popular';
 import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
 
 function Shop() {
   return (
@@ -9,6 +10,7 @@ function Shop() {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollections/>
     </div>
   )
 }

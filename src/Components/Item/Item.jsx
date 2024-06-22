0693b644b98fd819c6394c554152d6b86ccefc56
theprithwisingh <prithwisingh77@gmail.com>
2024-06-22 20:47:35 +0530

    components/pages

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -7,10 +7,10 @@ function Item(props) {
         <p>{props.name}</p>
         <div className='item-prices'>
             <div className='item-price-new'>
-                {props.old_price}
+                ${props.old_price}
             </div>
             <div className='item-price-old'>
-                {props.old_price}
+                ${props.old_price}
             </div>
         </div>
     </div>

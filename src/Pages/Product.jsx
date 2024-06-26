9588d34c92d29210f2436e645c9642ce60b2fcb0
theprithwisingh <prithwisingh77@gmail.com>
2024-06-26 23:51:05 +0530

    product/productdisplay/breadcrum

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useContext}from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import all_product from '../Components/Assets/all_product';
+import {useParams} from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 function Product() {
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product  = all_product.find((e)=>e.id===Number(productId));
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
+    </div>
   )
 }
 
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -4,11 +4,11 @@
 // import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 // import Item from '../Components/Item/Item'
 
-// function ShopCategory(props) {
+// function ShopCategory({ banner, category }) {
 //   const {all_product} = useContext(ShopContext)
 //   return (
 //     <div className='shop-category'>
-//        <img src={props.banner} alt="" />
+//        <img src={banner} alt="" />
 //        <div className='shopcategory-indexSort'>
 //          <p>
 //           <span>Showing 1-12</span> out of 36 products
@@ -20,8 +20,8 @@
 //        <div className='shopcategory-products'>
 //          {
 //           all_product.map((item,i)=>{
-//             if (props.category===Item.category) {
-//               return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+//             if (category===Item.category) {
+//               return<Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
 //             }
 //             else {
 //               return null}
